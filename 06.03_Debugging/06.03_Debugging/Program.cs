65b50431c4ebb74e2bfbcd7924536fa9c960abc9
Petr Tinka <tinka14@seznam.cz>
2020-02-19 13:15:38 +0100

    Debugging

---
+++ 06.03_Debugging/06.03_Debugging/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06._03_Debugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] list = { 10, 50, 30, 140, 20, 3, 60, 80 };
+            Console.WriteLine(GetMax(list));
+        }
+        static int GetMax(int[] list)
+        {
+            int MaxValue = list[0];
+            for (int i = 0; i < list.Length; i++)
+            {
+                if (list[i] > MaxValue)
+                {
+                    MaxValue = list[i];
+                }
+            }
+            return MaxValue;
+        }
+    }
+}
