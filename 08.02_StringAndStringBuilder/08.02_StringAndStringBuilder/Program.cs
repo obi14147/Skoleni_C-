69813fc50e05f880358fb88efd872e5f6608a73c
Petr Tinka <tinka14@seznam.cz>
2020-02-20 09:27:57 +0100

    String and string builder

---
+++ 08.02_StringAndStringBuilder/08.02_StringAndStringBuilder/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08._02_StringAndStringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string s1 = "ABC";
+            //string s2 = s1;
+            //Console.WriteLine(s1 == s2);
+            //s2 += "D";
+            //Console.WriteLine(s2);
+            //Console.WriteLine(s1);
+            //Console.WriteLine(s1==s2);
+
+            string s = "";
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+            Console.WriteLine("Start");
+            for (int i = 0; i < 100000; i++)
+            {
+                s += "A";
+            }
+            Console.WriteLine("Stop");
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds + "ms");
+
+            System.Diagnostics.Stopwatch ssw = new System.Diagnostics.Stopwatch();
+            ssw.Start();
+            Console.WriteLine("Start with SB");
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < 100000; j++)
+            {
+                sb.Append("A");
+            }
+            string ss = sb.ToString();
+            Console.WriteLine("Stop with SB");
+            ssw.Stop();
+            Console.WriteLine(ssw.ElapsedMilliseconds + "ms");
+
+
+        }
+    }
+}
