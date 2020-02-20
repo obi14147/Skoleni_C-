d1b68a8d01c355fa7c3ea31b9fc41d088623c7e1
Petr Tinka <tinka14@seznam.cz>
2020-02-20 11:11:14 +0100

    Finalizace

---
+++ 09.02_Finalizace/09.02_Finalizace/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09._02_Finalizace
+{
+    class FileStream
+    {
+        public void Open()
+        {
+            Console.WriteLine("Opening file");
+        }
+
+        public void Close()
+        {
+            Console.WriteLine("Closing file");
+        }
+        ~FileStream()
+        {
+            this.Close();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream();
+            //fs.Open();
+
+            try
+            {
+                fs.Open();
+                //Pracuji se souborem
+                throw new Exception("Invalid file operation");
+            }
+            finally
+            {
+                fs.Close();
+            }
+
+
+        }
+    }
+}
