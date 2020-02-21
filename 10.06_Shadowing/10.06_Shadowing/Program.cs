4e3b00bc86afb5a81ab26fe9d2e5aa80b1935758
Petr Tinka <tinka14@seznam.cz>
2020-02-21 10:02:46 +0100

    Shadowing

---
+++ 10.06_Shadowing/10.06_Shadowing/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_06_Shadowing
+{
+    class Animal
+    {
+        public string MakeSound()
+        {
+            return "";
+        }
+    }
+
+    class Fish : Animal
+    {
+
+    }
+
+    class Cat : Animal
+    {
+        new public string MakeSound()
+        {
+            return "Yow";
+        }
+    }
+
+    class PersianCat : Cat
+    {
+        new public string MakeSound()
+        {
+            return base.MakeSound() + " " + base.MakeSound();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fish fish = new Fish();
+            Cat cat = new Cat();
+            PersianCat persianCat = new PersianCat();
+
+            Animal[] animals = new Animal[3];
+            animals[0] = new Fish();
+            animals[1] = new Cat();
+            animals[2] = new PersianCat();
+            foreach (Animal item in animals)
+            {
+                if (item is PersianCat)
+                {
+                    Console.WriteLine(((PersianCat)item).MakeSound());
+                }
+                else if (item is Cat)
+                {
+                    Console.WriteLine(((Cat)item).MakeSound());
+                }
+                else
+                {
+                    Console.WriteLine(item.MakeSound());
+                }
+                MakeSoundOfAnimal(item);
+            }            
+        }
+        
+       
+        static void MakeSoundOfAnimal(Animal animal)
+        {
+            Console.WriteLine("Make sound: {0}", animal.MakeSound());
+        }
+    }
+}
--- README.md
+++ README.md
@@ -54,5 +54,7 @@ Použití virtuálních metod a overridingu.
 Přepisování metod datového typu System.Object (ToStrin, Equals...)
 ## 10.04_CastingAndBoxing
 Přetypovávání referenčních typů a boxování hodnotových typů.
-##10.05_CastingAndVirtualMethods
+## 10.05_CastingAndVirtualMethods
 Souvislost přetypovávání a virtuální metody.
+## 10.06_Shadowing
+Souvislost vytváření virtuální metody a zastinování. Zastinovaání je horší.
